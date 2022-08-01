81594f5262116755f1702ca7c2326491a64bffe9
Arsen <102597298+molchoun@users.noreply.github.com>
2022-08-01 23:39:16 +0400

    Display image

--- Gui Features in OpenCV/display_image.py
+++ Gui Features in OpenCV/display_image.py
@@ -1,10 +1,10 @@
 import sys
 import cv2 as cv
 
-img = cv.imread(cv.samples.findFile("starry_night.jpg"))
+img = cv.imread("./Gui Features in OpenCV/starry_night.jpg")
 if img is None:
     sys.exit("Could not read the image.")
 cv.imshow("Display window", img)
 k = cv.waitKey(0)
 if k == ord("s"):
-    cv.imwrite("starry_night.png", img)
+    cv.imwrite("./Gui Features in OpenCV/starry_night.jpg", img)
