520b0369fdd160c3854d2f4e0b5c8d5d19019f63
Arsen <102597298+molchoun@users.noreply.github.com>
2022-08-14 19:24:39 +0400

    Replicate OpenCV logo

--- Gui Features in OpenCV/open-cv-logo.py
+++ Gui Features in OpenCV/open-cv-logo.py
@@ -6,13 +6,13 @@ import cv2 as cv
 img = np.random.randint(254, 255, (512, 512, 3), np.uint8)
 
 # Green circle
-cv.ellipse(img, (205, 256), (50, 50), 0, 0, 300, (0, 180, 0), -1)
+cv.ellipse(img, (205, 256), (50, 50), 0, 0, 300, (0, 255, 0), -1)
 cv.ellipse(img, (205, 256), (25, 25), 0, 0, 360, (255, 255, 255), -1)
 # Blue circle
-cv.ellipse(img, (308, 256), (50, 50), 300, 0, 300, (180, 0, 0), -1)
+cv.ellipse(img, (308, 256), (50, 50), 300, 0, 300, (255, 0, 0), -1)
 cv.ellipse(img, (308, 256), (25, 25), 0, 0, 360, (255, 255, 255), -1)
 # Red circle
-cv.ellipse(img, (256, 165), (50, 50), 120, 0, 300, (0, 0, 180), -1)
+cv.ellipse(img, (256, 165), (50, 50), 120, 0, 300, (0, 0, 255), -1)
 cv.ellipse(img, (256, 165), (25, 25), 0, 0, 360, (255, 255, 255), -1)
 
 cv.imshow("Display image", img)
